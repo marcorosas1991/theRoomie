850eb603b9aae2487fb1234bcbf71d60901fa8f8
Marco Rosas <marco.rosas@outlook.com>
2015-04-23 20:41:11 -0600

    cit-

--- aux/header.php
+++ aux/header.php
@@ -9,8 +9,6 @@ echo '<header class="mainColorTheme">
     <a href="../index.html">Home</a>
     |
     <a href="../indv-website/support.html">Support</a>
-    |
-    <a href="../cit230-links.html">CIT230-links</a>
   </nav>
 </header>';
 
