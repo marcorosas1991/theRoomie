aca6f727ddb01ebcf2b5e2c1f7450b6d8b4b4aa4
Marco Rosas <marco.rosas@outlook.com>
2015-04-29 15:58:56 -0600

    exercises

--- public/exercises/index.php
+++ public/exercises/index.php
@@ -2,3 +2,57 @@
   <link rel="stylesheet" href="/css/indv-styles.css">
 </head>
 <h1>Exercises</h1>
+
+<a href="#">Exercise 1-1</a>
+
+<a href="#">Exercise 1-2</a>
+
+<a href="#">Exercise 1-3</a>
+
+<a href="#">Exercise 2-1</a>
+
+<a href="#">Exercise 3-1</a>
+
+<a href="#">Exercise 4-1</a>
+
+<a href="#">Exercise 5-1</a>
+
+<a href="#">Exercise 6-1</a>
+
+<a href="#">Exercise 6-2</a>
+
+<a href="#">Exercise 7-2</a>
+
+<a href="#">Exercise 8-2</a>
+
+<a href="#">Exercise 11-1</a>
+
+<a href="#">Exercise 13-1</a>
+
+<a href="#">Exercise 12-2</a>
+
+<a href="#">Exercise 15-1</a>
+
+<h1>Enhancements</h1>
+
+<a href="#">Enhancement 2-1</a>
+
+<a href="#">Enhancement 4-1</a>
+
+<a href="#">Enhancement 5-1</a>
+
+<a href="#">Enhancement 11-1</a>
+
+<a href="#">Enhancement 13-1</a>
+
+<a href="#">Enhancement 12-2</a>
+
+<h1>Exams</h1>
+
+<a href="#">Unit 2 Exam</a>
+
+<a href="#">Unit 3 Exam</a>
+
+<h1>Projects</h1>
+
+<a href="#">Project Proposal</a>
