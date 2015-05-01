e2cdc42bf949a8132f24f11bd1841b7ea8f9e258
Marco Rosas <marco.rosas@outlook.com>
2015-05-01 16:36:24 -0600

    week2

--- public/aux/header.php
+++ public/aux/header.php
@@ -1,12 +1,12 @@
 <?php
 
 echo '<header class="mainColorTheme">
-  <a href="/index.html">
+  <a href="/index.php">
     <div class="logoframe mainLogoFont">theRoomie</div>
   </a>
 
   <nav class="main">
-    <a href="/index.html">Home</a>
+    <a href="/index.php">Home</a>
     |
     <a href="/indv-website/support.html">Support</a>
   </nav>
