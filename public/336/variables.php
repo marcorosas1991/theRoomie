6d6f773ea6c85c0b6d60a35cf1d6b2fd597b057c
Marco Rosas <marco.rosas@outlook.com>
2015-05-04 15:50:12 -0600

    variable_assingment

---
+++ public/336/variables.php
@@ -0,0 +1,20 @@
+<!DOCTYPE HTML>
+<html>
+  <head>
+    <link rel="stylesheet" href="/css/style.css">
+    <title>PHP Variables</title>
+  </head>
+  <body>
+
+    <?php
+    $line1 = 'She said, "it\'s Spring."<br>';
+    $line2 = 'He replied, "So?"<br>';
+    $line3 = 'She said, "Thoughts are supposed to turn to love."<br>';
+    $line4 = 'Hesitantly, he responded, "Where is the headed?"<br>';
+    $line5 = 'She looked at him with a flat stare<br>';
+
+    echo "<p>$line1$line2$line3$line4$line5</p>";
+    ?>
+
+  </body>
+</html>
