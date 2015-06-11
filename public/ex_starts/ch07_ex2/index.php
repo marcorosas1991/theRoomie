3703210c16c0bae7cd0035bcd94f4832dde0dffc
Marco Rosas <marco.rosas@outlook.com>
2015-06-10 22:27:19 -0600

    exercise_7_2

--- public/ex_starts/ch07_ex2/index.php
+++ public/ex_starts/ch07_ex2/index.php
@@ -1,8 +1,8 @@
-<?php 
+<?php
     //set default value of variables for initial page load
-    if (!isset($investment)) { $investment = '10000'; } 
-    if (!isset($interest_rate)) { $interest_rate = '5'; } 
-    if (!isset($years)) { $years = '5'; } 
+    if (!isset($investment)) { $investment = '10000'; }
+    if (!isset($interest_rate)) { $interest_rate = '5'; }
+    if (!isset($years)) { $years = '5'; }
 ?>
 <!DOCTYPE html>
 <html>
@@ -21,12 +21,32 @@
 
         <div id="data">
             <label>Investment Amount:</label>
-            <input type="text" name="investment"
-                   value="<?php echo $investment; ?>"/><br>
+            <select name="investment">
+              <?php
+                for ($i = 1; $i <=5; $i++) {
+                  $amount = 10000 * $i;
+                  echo "
+                  <option value='$amount'>
+                    $amount
+                  </option>";
+                }
+              ?>
+            </select><br>
 
             <label>Yearly Interest Rate:</label>
-            <input type="text" name="interest_rate"
-                   value="<?php echo $interest_rate; ?>"/><br>
+            <select name="interest_rate">
+              <?php
+                for ($i = 4; $i <=12; $i++) {
+                  for ($j = 0; $j <=1; $j++) {
+                    $interest = $i + $j * 0.5;
+                    echo "
+                    <option value='$interest'>
+                      $interest
+                    </option>";
+                  } // $j
+                } // $i
+              ?>
+            </select><br>
 
             <label>Number of Years:</label>
             <input type="text" name="years"
